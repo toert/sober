c4eb42a79e8aba038d29d0be50b6656fda3c1e6a
Пользователь <MacBook@Users-MacBook-Pro.local>
2017-05-08 12:21:45 +0300

    change models

--- bot/models.py
+++ bot/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import re
 
 TYPES_OF_AD = (
     ('buy', 'Обьявление о продаже клиенту'),
@@ -6,8 +7,15 @@ TYPES_OF_AD = (
 )
 
 
+class LocalUser(models.Model):
+    ad_id = models.CharField(max_length=40, db_index=True, verbose_name='Логин')
+    hmac_key = models.CharField(max_length=140, db_index=True, verbose_name='HMAC ключ')
+    hmac_secret = models.CharField(max_length=140, verbose_name='HMAC пароль')
+
+
 class Ad(models.Model):
 
+    user = models.ForeignKey(LocalUser, related_name='ads', verbose_name='Пользователь-владелец')
     ad_id = models.CharField(max_length=40, db_index=True, verbose_name='ID объявления')
     price_equation = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена', default=0.10)
     direction = models.CharField(max_length=80, choices=TYPES_OF_AD, verbose_name='Тип обьявления')
@@ -25,11 +33,22 @@ class Ad(models.Model):
     ad_creation_time_filter = models.DateTimeField(verbose_name='T игнор')
     step = models.IntegerField(verbose_name='Шаг цены')
     steps_quantity = models.IntegerField(verbose_name='Количество шагов')
+    ignored_logins = models.TextField(verbose_name='Игнорируемые логины', null=True)
+    invisible_logins = models.TextField(verbose_name='Логины-невидимки', null=True)
     price_rollback = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Откат цены (откат)')
     rollback_time = models.IntegerField(verbose_name='Время отката (T откат) в секундах')
     call_frequency = models.IntegerField(verbose_name='Частота бота в секундах')
     top_fifteen = models.BooleanField(verbose_name='Топ 15')
-    visible = models.BooleanField(verbose_name='Состояние объявления')
+    is_visible = models.BooleanField(verbose_name='Включено ли объявление?', default=False)
+    is_updated = models.BooleanField(verbose_name='Обновлять ли объявление?', default=False)
+
+    def get_ignored_logins_as_list(self):
+        list_of_logins = re.findall(r'([^, ]+)', str(self.ignored_logins))
+        return list_of_logins
+
+    def get_invisible_logins_as_list(self):
+        list_of_logins = re.findall(r'([^, ]+)', str(self.invisible_logins))
+        return list_of_logins
 
     def __str__(self):
         return '{}/{}/{}'.format(self.ad_id, self.direction, self.price_equation)
@@ -43,16 +62,3 @@ class CurrentInfo(models.Model):
 
     def __str__(self):
         return '{}-{}-{}'.format(self.ad.ad_id, self.current_ad_position, self.current_step)
-
-
-class IllegalLogin(models.Model):
-
-    ad = models.ForeignKey(Ad, related_name='Игнорируемые_логины', db_index=True)
-    attribute_value = models.CharField(max_length=200, verbose_name='Игнорируемые логины')
-
-    class Meta:
-        verbose_name = 'Ключевое слово'
-        verbose_name_plural = 'Ключевые слова'
-
-    def __str__(self):
-        return '{}-{}'.format(self.ad.ad_id, self.attribute_value)
