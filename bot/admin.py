484d6e529c986bd7c084a2544ce1850392cb9119
toert <MacBook@rfc1918.private.ip.localhost>
2017-05-11 14:50:12 +0300

    add first template

--- bot/admin.py
+++ bot/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Ad, LocalUser
+from django.contrib.auth.models import User
 
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
 ]
